e66b2ea8d81e6aec81e4d30f25fa7409d300f39d
Gisle Aas <gisle@aas.no>
1998-01-09 02:23:52 +0000

    Reformatting.

--- SSL.pm
+++ SSL.pm
@@ -10,14 +10,24 @@ $VERSION = '0.01';
 
 bootstrap SSL $VERSION;
 
-package SSL::CTX;
+use vars qw(%CIPHERS);
+%CIPHERS = (
+   'NULL-MD5'     => "No encryption with a MD5 MAC",
+   'RC4-MD5'      => "128 bit RC4 encryption with a MD5 MAC",
+   'EXP-RC4-MD5'  => "40 bit RC4 encryption with a MD5 MAC",
+   'RC2-CBC-MD5'  => "128 bit RC2 encryption with a MD5 MAC",
+   'EXP-RC2-CBC-MD5' => "40 bit RC2 encryption with a MD5 MAC",
+   'IDEA-CBC-MD5' => "128 bit IDEA encryption with a MD5 MAC",
+   'DES-CBC-MD5'  => "56 bit DES encryption with a MD5 MAC",
+   'DES-CBC-SHA'  => "56 bit DES encryption with a SHA MAC",
+   'DES-CBC3-MD5' => "192 bit EDE3 DES encryption with a MD5 MAC",
+   'DES-CBC3-SHA' => "192 bit EDE3 DES encryption with a SHA MAC",
+   'DES-CFB-M1'   =>   "56 bit CFB64 DES encryption with a one byte MD5 MAC",
+);
 
-sub DESTROY { shift->free; }
 
-package SSL::Conn;
-
-sub DESTROY { shift->free; }
-
-package SSL;
+# A xsupp bug made this nessesary
+sub SSL::CTX::DESTROY  { shift->free; }
+sub SSL::Conn::DESTROY { shift->free; }
 
 1;
